1f24a304e8a25f7442838786014f79e7085ac4cf
Gabati <geibrus.00@gmail.com>
2022-03-09 13:10:13 +0100

    Texto mucho
    Signed-off-by: Gabati <geibrus.00@gmail.com>

--- Git/src/hola/Hola.java
+++ Git/src/hola/Hola.java
@@ -5,6 +5,9 @@ public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 
+		System.out.println("Mocaco");
+		System.out.println("Dinopolis");
+		System.out.println("Avestruz");
 	}
 
 }
